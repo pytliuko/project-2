470e1b915afbb182366bea6d2259b14ef14d4a54
Oleh <oleh.pytliuk@auk.edu.ua>
2025-09-11 01:05:05 +0300

    Changes

--- src/part2_chatbot/Main.java
+++ src/part2_chatbot/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // For this small chatbot, I feel that the use of if/else statements is inevitable.
 
         System.out.println("Welcome! Let's chat!");
-        // We'll go the extra mile here and use the 'while' loop to consistently display You: before every message.
+        // We'll go the extra mile here and use the 'while' loop to consistently display You:.
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine();
@@ -26,6 +26,7 @@ public class Main {
                     userInput.toLowerCase().contains("let's do it")) {
 
                 System.out.println("ChatBot: Great! How may I address you?");
+                System.out.print("You: ");
 
                 String nameInput = scanner.nextLine();
 
@@ -34,6 +35,15 @@ public class Main {
                     System.out.println("ChatBot: Nice to meet you, " + name + "!");
                 } else {
                     System.out.println("ChatBot: Oh, could you start with 'My name is'? I'm a little silly at recognizing names by ear.");
+
+                    System.out.print("You: ");
+                    nameInput = scanner.nextLine();
+                    if (nameInput.startsWith("My name is ")) {
+                        String name = nameInput.replace("My name is ", "");
+                        System.out.println("ChatBot: Nice to meet you, " + name + "!");
+                    } else {
+                        System.out.println("ChatBot: Alright, I'll just call you Untitled_1!");
+                    }
                 }
             }
 
@@ -41,12 +51,14 @@ public class Main {
             else if (userInput.startsWith("My name is")) {
                 String name = userInput.replace("My name is ", "");
                 System.out.println("ChatBot: Nice to meet you, " + name + "!");
+
             }
 
             System.out.println("ChatBot: How are you feeling today?");
+            System.out.print("You: ");
             String feeling = scanner.nextLine();
 
-
+            // We'll introduce lowercase here because we won't normally use Caps anyway
             if (feeling.toLowerCase().contains("good") ||
                     feeling.toLowerCase().contains("great") ||
                     feeling.toLowerCase().contains("i'm fine, thanks") ||
@@ -57,8 +69,32 @@ public class Main {
                 System.out.println("ChatBot: Oh well. I hope it gets better!");
             }
 
+            System.out.println("ChatBot: Do you have a hobby?");
+            System.out.print("You: ");
+            String hobby = scanner.nextLine();
 
+            if (hobby.toLowerCase().contains("bike riding") ||
+                    hobby.toLowerCase().contains("philosophy") ||
+                    hobby.toLowerCase().contains("basketball") ||
+                    hobby.toLowerCase().contains("football") ||
+                    hobby.toLowerCase().contains("math") ||
+                    hobby.toLowerCase().contains("coding") ||
+                    hobby.toLowerCase().contains("music")) {
 
+                System.out.println("ChatBot: That's so cool! I used to love " + hobby + " back when I was younger... But that was tens of years ago.");
+            } else {
+                System.out.println("ChatBot: Sounds fancy! Always nice to see talented youngsters like you. I should try that sometime, too.");
+            }
+
+            System.out.println("ChatBot: Do you have any pets?");
+            System.out.print("You: ");
+            String pets = scanner.nextLine();
+
+            if (pets.toLowerCase().contains("cat")) {
+                System.out.println("ChatBot: Oh Lord. I used to throw my cat around the house and squeeze him when I was a kid. That's one thing I'll never understand about myself.");
+            } else {
+                System.out.println("ChatBot: Pets are always fun to have, man. Hope they bring happiness to you.");
+            }
         }
 
         scanner.close();
